edf8ca3046e7f96b936964cac66d200ca5ebfffa
mohanrengaraju <mohanrengaraju2451.email@example.com>
2025-04-12 12:15:40 +0530

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/calculations', calculationRoutes);
 
-mongoose.connect('mongodb://localhost:27017/calculatorDB')
+mongoose.connect('mongodb+srv://mohan:Mohan%401234@cluster0.j1riart.mongodb.net/calculator')
   .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
   .catch(err => console.error(err));
